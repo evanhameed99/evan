b8d46169245d1705dba0163d76bed58dab92032d
evanhameed99 <evanhameed9966@gmail.com>
2022-04-09 20:33:42 +0300

    change

--- src/main.js
+++ src/main.js
@@ -6,10 +6,15 @@ const Main = () => {
 
       <h3 className='name'>Evan Hameed Saeed</h3>  
 
-      <p className='description'>Software engineer with hands-on expereince in the software development lifecycle. I have worked
-       with a variety of software technologies and principles.
-        Higlhy expereinced with building interactve user interfaces using the latest web frameworks
-        such as React and Redux as a state management tool.<br/><br/> Utilized and worked on the implementaiton and optimization process of RESTful APIs using Node.js and Express.js. Hands-on expereicne working and structuring relational and non-relational databases such as MySQL and MongoDB. Expereince with caching services such as Redis. <br/> <br/> Background on Servers hosting, deployments, EC2s, CI/CDs, Lambda, jenkins, and Docker. Worked with AWS services such as SQS, SNS, S3 and many more. <br/> <br/> hands-on experience working on SaaS products. Interested in everything related to Technology and product vision.</p>
+      <p className='description'>
+      Software engineer with hands-on experience in the software development lifecycle. I have worked with a variety of software technologies and principles. Highly experienced with building interactive user interfaces using the latest web frameworks such as React and Redux as state management tool. <br/> <br/>
+
+Utilized and worked on the implementation and optimization process of RESTful APIs using Node.js and Express.js. Hands-on experience working and structuring relational and non-relational databases such as MySQL and MongoDB. Experience with caching services such as Redis.
+
+<br/> <br/>Background on Server hosting, deployments, EC2s, CI/CDs, Lambda, Jenkins, and Docker. Worked with AWS services such as SQS, SNS, S3, and many more.
+Worked with queueing systems such as RabbitMQ, Redis, and many more.
+Highly knowledged working on SaaS products. <br/> <br/>Interested in everything related to Technology and product vision.
+      </p>
 
        <div style={{paddingTop: '2rem'}}>
        <SocialIcon url="https://github.com/evanhameed99" network='github' bgColor='white' style={{marginRight:'20px'}}/>   
